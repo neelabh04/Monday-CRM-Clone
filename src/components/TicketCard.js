6ef1c6ca5ec66cbee55bc3300fe75e9fe159be7f
Neelabh Singh <neelabh041997@gmail.com>
2022-10-16 15:44:01 +0530

    Ticket-Color and Avatar-display basic flow

--- src/components/TicketCard.js
+++ src/components/TicketCard.js
@@ -9,10 +9,10 @@ const TicketCard = ({ color, ticket }) => {
   return (
     <div className="ticket-card">
       <Link to={`/ticket/${ticket.documentId}`} id="link">
-        <div className="ticket-color"></div>
+      <div className="ticket-color"></div>
         <h3>{ticket.title}</h3>
-        <AvatarDisplay />
-        <StatusDisplay />
+        <AvatarDisplay ticket={ticket}/>
+        <StatusDisplay status={ticket.status}/>
         <PriorityDisplay />
         <ProgressDisplay />
       </Link>
