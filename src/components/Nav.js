f94c64f0213c0c0f2e5faff6500d183af47631a8
Neelabh Singh <neelabh041997@gmail.com>
2022-10-16 15:14:50 +0530

    Basic layout with ticket filtering and grid border

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,23 @@
+import logo from "../images/crm-logo.png";
+import { useNavigate } from "react-router-dom";
+
 const Nav = () => {
-    return <div>Nav</div>
-}
+  const navigate = useNavigate();
+  return (
+    <nav>
+      <div className="logo-container">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="controls-container">
+        <div className="icon" onClick={() => navigate("/ticket")}>
+          ＋
+        </div>
+        <div className="icon" onClick={() => navigate("/")}>
+          ❮❮
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,77 @@
+import TicketCard from "../components/TicketCard";
+
 const Dashboard = () => {
-    return <div>Dashboard</div>
-}
+  const tickets = [
+    {
+      category: "Q1 2022",
+      color: "red",
+      title: "NFT Safety 101 Video",
+      owner: "Ania Kubow",
+      avatar:
+        "https://media-exp1.licdn.com/dms/image/C5603AQHz0XsNvXLBhw/profile-displayphoto-shrink_400_400/0/1610123921178?e=1671667200&v=beta&t=a6m1xm8f2Kte0nzH3a_qrDhIx_y6QYPZ5OaC1iKORwc",
+      status: "done",
+      priority: 5,
+      progress: 40,
+      description:
+        "Make a video showcasing how to work with NFTs safely, including how to know if one is not genuine.",
+      timestamp: "2022-02-11T07:36:17+0000",
+    },
+    {
+      category: "Q1 2022",
+      color: "red",
+      title: "Build and sell AI Model",
+      owner: "Ania Kubow",
+      avatar:
+        "https://media-exp1.licdn.com/dms/image/C5603AQHz0XsNvXLBhw/profile-displayphoto-shrink_400_400/0/1610123921178?e=1671667200&v=beta&t=a6m1xm8f2Kte0nzH3a_qrDhIx_y6QYPZ5OaC1iKORwc",
+      status: "working on it",
+      priority: 5,
+      progress: 70,
+      description: "Make a video about AI",
+      timestamp: "2022-02-13T07:36:17+0000",
+    },
+    {
+      category: "Q2 2022",
+      color: "blue",
+      title: "Build a bot",
+      owner: "Ania Kubow",
+      avatar:
+        "https://media-exp1.licdn.com/dms/image/C5603AQHz0XsNvXLBhw/profile-displayphoto-shrink_400_400/0/1610123921178?e=1671667200&v=beta&t=a6m1xm8f2Kte0nzH3a_qrDhIx_y6QYPZ5OaC1iKORwc",
+      status: "working on it",
+      priority: 3,
+      progress: 10,
+      description: "Make a video about creating a bot",
+      timestamp: "2022-02-15T07:36:17+0000",
+    },
+  ];
+
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
+
+  console.log(uniqueCategories);
+
+  return (
+    <div className="dashboard">
+      <h1>My Projects</h1>
+      <div className="ticket-container">
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex}>
+              <h3>{uniqueCategory}</h3>
+              {tickets
+                .filter((ticket) => ticket.category === uniqueCategory)
+                .map((filteredTicket, _index) => (
+                  <TicketCard 
+                    id={_index}
+                    color={filteredTicket.color}
+                    ticket={filteredTicket}
+                  />
+                ))}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
